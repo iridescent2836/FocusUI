0dadbd4651afbdefad865e4f336910cdbda23b2d
iridescent2836 <464935069@qq.com>
2026-05-15 19:07:38 +0800

    tmp

--- eval_qwen25vl_3b_parallel_ui-graph-score.sh
+++ eval_qwen25vl_3b_parallel_ui-graph-score.sh
@@ -2,8 +2,8 @@
 UI_GROUNDING_BENCH_BASE_DIR="datasets/UI-Grounding-Benchmarks"
 
 MODEL_TYPE="focusui_3b"
-# MODEL_PATH="checkpoints/focusui_3b_ft_scorer"
-MODEL_PATH="checkpoints/FocusUI-3B"
+MODEL_PATH="checkpoints/focusui_3b_ft_scorer"
+# MODEL_PATH="checkpoints/FocusUI-3B"
 SAVE_PATH="eval_results/focusui_3b/ScreenSpot-V2_e2e_latency"
 EVAL_VISUAL_REDUCT_RATIOS="0.70"
 SCORER_TYPE="l2-norm"
@@ -15,22 +15,24 @@ SCORER_TYPE="l2-norm"
 
 echo "ScreenSpot-V2 | drop=${EVAL_VISUAL_REDUCT_RATIOS} | scorer=${SCORER_TYPE} | end to end latency | parallel computation"
 python -m evaluation.ss_v2_eval \
-    --save_path "${SAVE_PATH}/drop_${drop}_${SCORER_TYPE}_${MODEL_TYPE}_parallel" \
+    --save_path "${SAVE_PATH}/drop_${EVAL_VISUAL_REDUCT_RATIOS}_${SCORER_TYPE}_${MODEL_TYPE}_parallel" \
     --visual_reduct_ratio "${EVAL_VISUAL_REDUCT_RATIOS}" \
     --scorer_type "${SCORER_TYPE}" \
     --model_type "${MODEL_TYPE}" \
     --data_path "${UI_GROUNDING_BENCH_BASE_DIR}/ScreenSpot-V2" \
     --model_name_or_path "${MODEL_PATH}" \
-    --using_random_samples
+    --using_combined_scorer
+
 
 echo "ScreenSpot-V2 | drop=${EVAL_VISUAL_REDUCT_RATIOS} | scorer=${SCORER_TYPE} | end to end latency |  sequential computation"
 python -m evaluation.ss_v2_eval \
-    --save_path "${SAVE_PATH}/drop_${drop}_${SCORER_TYPE}_${MODEL_TYPE}_sequential" \
+    --save_path "${SAVE_PATH}/drop_${EVAL_VISUAL_REDUCT_RATIOS}_${SCORER_TYPE}_${MODEL_TYPE}_sequential" \
     --visual_reduct_ratio "${EVAL_VISUAL_REDUCT_RATIOS}" \
     --scorer_type "${SCORER_TYPE}" \
     --model_type "${MODEL_TYPE}" \
     --data_path "${UI_GROUNDING_BENCH_BASE_DIR}/ScreenSpot-V2" \
     --model_name_or_path "${MODEL_PATH}" \
+    --using_combined_scorer \
     --using_sequential_computation
 
 
